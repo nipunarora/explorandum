8105575a31f15d599aba747da24079f164e6d169
Comedyboy19 <Comedyboy19@1c0cb4e8-a307-11de-bb10-370f7287a6b7>
2009-09-20 01:28:29 +0000

    added newplayer package

---
+++ trunk/explorandum/src/explorandum/newplayer/Cell.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package explorandum.newplayer;
+
+import java.awt.Point;
+
+/**
+ *
+ * @author danny
+ */
+public class Cell {
+
+    private Point point;
+    private int status;
+    private int rank;
+    
+}
